f607133fdf6f4ecd5b4ae71fc82a64adfc57c171
alexnaryshkin <alexqqq100@gmail.com>
2022-06-02 12:56:10 -0400

    m

---
+++ src/main/java/com/example/restservice/Controller.java
@@ -0,0 +1,26 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/ui/hey/{param}")
+    public String checkEndpoint(@PathVariable("param") String p)
+    {
+        return "Hey";
+    }
+}
